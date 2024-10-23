0c4196f7d2015a6b2eccfc06eb8c2bbfa6444f9f
joaopcarvalho21 <joaopcarvalho21@GMAIL.COM>
2024-10-23 00:03:21 -0300

    Final

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,60 +6,98 @@ import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         Curso curso1 = new Curso();
-        curso1.setTitulo("curso java");
-        curso1.setDescricao("descrição curso java");
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Aprendizado sobre Java");
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso2.setTitulo("curso js");
-        curso2.setDescricao("descrição curso js");
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Aprendizado sobre JavaScript");
         curso2.setCargaHoraria(4);
 
-        Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("mentoria de java");
-        mentoria.setDescricao("descrição mentoria java");
-        mentoria.setData(LocalDate.now());
+        Curso curso3 = new Curso();
+        curso3.setTitulo("Curso Spring Boot");
+        curso3.setDescricao("Aprendizado sobre Spring Boot");
+        curso3.setCargaHoraria(10);
 
-        /*System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);*/
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Mentoria focada em boas práticas de Java");
+        mentoria1.setData(LocalDate.now());
 
         Bootcamp bootcamp = new Bootcamp();
-        bootcamp.setNome("Bootcamp Java Developer");
-        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.setNome("Bootcamp Fullstack Developer");
+        bootcamp.setDescricao("Bootcamp focado em desenvolvimento Fullstack");
         bootcamp.getConteudos().add(curso1);
         bootcamp.getConteudos().add(curso2);
-        bootcamp.getConteudos().add(mentoria);
+        bootcamp.getConteudos().add(curso3);
+        bootcamp.getConteudos().add(mentoria1);
 
-        Dev devCamila = new Dev();
-        devCamila.setNome("Camila");
-        devCamila.inscreverBootcamp(bootcamp);
-        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
-        devCamila.progredir();
-        devCamila.progredir();
-        System.out.println("-");
-        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
-        System.out.println("Conteúdos Concluídos Camila:" + devCamila.getConteudosConcluidos());
-        System.out.println("XP:" + devCamila.calcularTotalXp());
+        List<Dev> devs = new ArrayList<>();
 
-        System.out.println("-------");
+        try {
+            System.out.println("Quantos alunos deseja inscrever no bootcamp?");
+            int numDevs = scanner.nextInt();
+            scanner.nextLine();
 
-        Dev devJoao = new Dev();
-        devJoao.setNome("Joao");
-        devJoao.inscreverBootcamp(bootcamp);
-        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
-        devJoao.progredir();
-        devJoao.progredir();
-        devJoao.progredir();
-        System.out.println("-");
-        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
-        System.out.println("Conteúdos Concluidos João:" + devJoao.getConteudosConcluidos());
-        System.out.println("XP:" + devJoao.calcularTotalXp());
+            if (numDevs <= 0) {
+                throw new IllegalArgumentException("O número de alunos deve ser maior que zero.");
+            }
 
+            for (int i = 0; i < numDevs; i++) {
+                System.out.println("Insira o nome do aluno " + (i + 1) + ":");
+                String nomeDev = scanner.nextLine();
+
+                Dev dev = new Dev();
+                dev.setNome(nomeDev);
+                dev.inscreverBootcamp(bootcamp);
+                devs.add(dev);
+
+                System.out.println("Quantos conteúdos o aluno " + nomeDev + " vai progredir?");
+                int numConteudosProgredidos = scanner.nextInt();
+                scanner.nextLine();
+
+                if (numConteudosProgredidos < 0) {
+                    throw new IllegalArgumentException("O número de conteúdos progredidos não pode ser negativo.");
+                }
+
+                for (int j = 0; j < numConteudosProgredidos; j++) {
+                    dev.progredir();
+                }
+
+                System.out.println("Conteúdos Inscritos " + nomeDev + ": " + dev.getConteudosInscritos());
+                System.out.println("Conteúdos Concluídos " + nomeDev + ": " + dev.getConteudosConcluidos());
+                System.out.println("XP Total " + nomeDev + ": " + dev.calcularTotalXp());
+                System.out.println("Progresso " + nomeDev + ": " + dev.calcularProgresso() + "%");
+                System.out.println("-------");
+            }
+
+        } catch (InputMismatchException e) {
+            System.err.println("Erro: Entrada inválida! Por favor, insira números onde for solicitado.");
+            scanner.nextLine();
+
+        } catch (IllegalArgumentException e) {
+            System.err.println("Erro: " + e.getMessage());
+
+        } catch (NoSuchElementException e) {
+            System.err.println("Erro: Não há mais conteúdos para progredir!");
+
+        } catch (Exception e) {
+            System.err.println("Erro inesperado: " + e.getMessage());
+
+        } finally {
+            System.out.println("Lista de alunos inscritos no Bootcamp:");
+            for (Dev dev : devs) {
+                System.out.println(dev.getNome() + " - Progresso: " + dev.calcularProgresso() + "%");
+            }
+            scanner.close();
+        }
     }
-
 }
--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -7,34 +7,26 @@ public class Dev {
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootcamp(Bootcamp bootcamp){
+    public void inscreverBootcamp(Bootcamp bootcamp) {
         this.conteudosInscritos.addAll(bootcamp.getConteudos());
         bootcamp.getDevsInscritos().add(this);
     }
 
     public void progredir() {
         Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
-        if(conteudo.isPresent()) {
-            this.conteudosConcluidos.add(conteudo.get());
-            this.conteudosInscritos.remove(conteudo.get());
-        } else {
-            System.err.println("Você não está matriculado em nenhum conteúdo!");
-        }
+        conteudo.ifPresentOrElse(c -> {
+            this.conteudosConcluidos.add(c);
+            this.conteudosInscritos.remove(c);
+        }, () -> System.err.println("Você não está matriculado em nenhum conteúdo!"));
     }
 
     public double calcularTotalXp() {
-        Iterator<Conteudo> iterator = this.conteudosConcluidos.iterator();
-        double soma = 0;
-        while(iterator.hasNext()){
-            double next = iterator.next().calcularXp();
-            soma += next;
-        }
-        return soma;
+        return this.conteudosConcluidos.stream().mapToDouble(Conteudo::calcularXp).sum();
+    }
 
-        /*return this.conteudosConcluidos
-                .stream()
-                .mapToDouble(Conteudo::calcularXp)
-                .sum();*/
+    public int calcularProgresso() {
+        int total = conteudosConcluidos.size() + conteudosInscritos.size();
+        return (conteudosConcluidos.size() * 100) / total;
     }
 
 
